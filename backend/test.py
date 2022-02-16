a624cb2cfa84b6c6d8677866f7d4f6195ecdef5f
王伟 <yiluoAK_47@163.com>
2022-02-16 16:05:43 +0800

    修改拉库的目录

--- backend/test.py
+++ backend/test.py
@@ -6,13 +6,13 @@ from PIL import Image
 from torch import hub
 
 # 修改torch的缓存路径，避免windows下因盘符问题导致的异常
-hub._hub_dir = '../../.cache'
+hub._hub_dir = '../.cache'
 # Model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5m, yolov5l, yolov5x, custom
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', force_reload=True)  # or yolov5m, yolov5l, yolov5x, custom
 
 # Images
 # img = 'https://ultralytics.com/images/zidane.jpg'  # or file, Path, PIL, OpenCV, numpy, list
-img = '../../.cache/ultralytics_yolov5_master/data/images/zidane.jpg'
+img = '../.cache/ultralytics_yolov5_master/data/images/zidane.jpg'
 # Inference
 results = model(img)
 
@@ -22,7 +22,7 @@ results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
 print(results.pandas().xyxy[0].to_json(orient="records"))
 
 # 打印base64
-results.imgs # array of original images (as np array) passed to model for inference
+results.imgs  # array of original images (as np array) passed to model for inference
 results.render()  # updates results.imgs with boxes and labels
 for img in results.imgs:
     buffered = BytesIO()
--- backend/yolov5_model_init.py
+++ backend/yolov5_model_init.py
@@ -1,6 +1,6 @@
 # 修改torch的缓存路径，避免windows下因盘符问题导致的异常
 import torch
 
-torch.hub._hub_dir = '../../.cache'
+torch.hub._hub_dir = '../.cache'
 # Model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5m, yolov5l, yolov5x, custom
