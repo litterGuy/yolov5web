c841895e5b189e3bdf8e7c6d791236c096d61bf4
王伟 <yiluoAK_47@163.com>
2022-04-01 09:34:44 +0800

    add inference settings

--- backend/yolov5_model_init.py
+++ backend/yolov5_model_init.py
@@ -13,3 +13,17 @@ if os.path.exists('../.cache/ultralytics_yolov5_master'):
                        source='local')  # or yolov5m, yolov5l, yolov5x, custom
 else:
     model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5m, yolov5l, yolov5x, custom
+
+
+# Inference Settings
+# YOLOv5 models contain various inference attributes such as confidence threshold, IoU threshold, etc. which can be set by:
+
+# model.conf = 0.25  # NMS confidence threshold
+#       iou = 0.45  # NMS IoU threshold
+#       agnostic = False  # NMS class-agnostic
+#       multi_label = False  # NMS multiple labels per box
+#       classes = None  # (optional list) filter by class, i.e. = [0, 15, 16] for COCO persons, cats and dogs
+#       max_det = 1000  # maximum number of detections per image
+#       amp = False  # Automatic Mixed Precision (AMP) inference
+#
+# results = model(im, size=320)  # custom inference size
